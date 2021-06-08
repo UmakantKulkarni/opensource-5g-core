5da19a045e6bfc65d6ddd17e921f4c6fec3c5ad8
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-06-07 23:19:18 -0400

    script changes

--- helm-chart/Dockerfiles/ueransim/runSim.sh
+++ helm-chart/Dockerfiles/ueransim/runSim.sh
@@ -6,7 +6,7 @@ run_loop () {
   for i in $(seq 1 $numSubs);
   do
     imsi=$((imsi+1))
-    ./nr-ue -c ../config/ue.yaml -i $imsi
+    ./nr-ue -c ../config/ue.yaml -i $imsi > /dev/null 2>&1 & 
     echo "Created subscriber #$i with imsi $imsi"
     sleep 0.1
   done
