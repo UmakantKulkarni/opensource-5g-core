3334f2017193b86f0c3505ebe827f26971e667e8
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2021-06-07 23:34:17 -0400

    docker changes

--- helm-chart/Dockerfiles/ueransim/runSim.sh
+++ helm-chart/Dockerfiles/ueransim/runSim.sh
@@ -2,11 +2,11 @@
 numSubs="$1"
 imsi=208930000000000
 run_loop () {
-  cd /users/umakant/UERANSIM/build
+  #cd /users/umakant/UERANSIM/build
   for i in $(seq 1 $numSubs);
   do
     imsi=$((imsi+1))
-    ./nr-ue -c ../config/ue.yaml -i $imsi > /dev/null 2>&1 & 
+    nr-ue -c ../config/ue.yaml -i $imsi > /dev/null 2>&1 & 
     echo "Created subscriber #$i with imsi $imsi"
     sleep 0.1
   done
